e309488e9f07d3b36264ed16a491b59c1f359bff
Harris <harris.k1452@gmail.com>
2024-04-23 17:08:52 +0300

    Notable Skills

--- components/NotableSkills.js
+++ components/NotableSkills.js
@@ -7,15 +7,16 @@ const NotableSkills = () => {
     { skill: 'Greek', proficiency: 100},
     { skill: 'English', proficiency: 100 },
     { skill: 'Serbian', proficiency: 100},
-    { skill: 'AutoCAD 2023', proficiency: 100 },
-    { skill: 'Photoshop 2023', proficiency: 100},
-    { skill: 'Sketchup 2021 Pro', proficiency: 100 },
-    { skill: 'Lumion 12.5', proficiency: 100 },
-    { skill: 'Illustrator 2023', proficiency: 60 },
+    { skill: 'AutoCAD 2025', proficiency: 100 },
+    { skill: 'Photoshop 2024', proficiency: 100},
+    { skill: 'Sketchup 2024 Pro', proficiency: 100 },
+    { skill: 'Lumion 2023', proficiency: 100 },
+    { skill: 'Illustrator 2024', proficiency: 60 },
     { skill: 'ArchiCAD 26', proficiency: 50 },
-    { skill: 'Rhino 7', proficiency: 40 },
-    { skill: 'Blender', proficiency: 50 },
+    { skill: 'Rhino 8', proficiency: 40 },
+    { skill: 'Blender 4.2', proficiency: 50 },
     { skill: 'Web Development', proficiency: 40 },
+    { skill: 'Premiere Pro 2024 / Video Editing ', proficiency: 80 },
 
   ];
 
